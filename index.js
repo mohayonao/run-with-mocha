be9c27999a8246cf85816ab434c08c13c69c038a
mohayonao <mohayonao@gmail.com>
2016-10-20 21:35:09 +0900

    clean up code

--- index.js
+++ index.js
@@ -3,10 +3,20 @@
 const path = require("path");
 
 const MOCHA_GLOBAL_API = [ "context", "describe", "it", "before", "after", "beforeEach", "afterEach" ];
-
 const ENV = {};
 
-function findMocha() {
+function isMocha() {
+  return MOCHA_GLOBAL_API.every(key => typeof global[key] === "function");
+}
+
+function setupMochaAPI() {
+  MOCHA_GLOBAL_API.forEach((key) => {
+    global[key] = () => {};
+    global[key].skip = global[key].only = () => {};
+  });
+}
+
+function findMochaCommand() {
   try {
     const mochaPath = require.resolve("mocha");
     const pathItems = mochaPath.split(path.sep);
@@ -20,31 +30,24 @@ function findMocha() {
 }
 
 function runWithMocha() {
-  if (MOCHA_GLOBAL_API.every(key => typeof global[key] === "function")) {
-    return;
-  }
-  MOCHA_GLOBAL_API.forEach((key) => {
-    global[key] = () => {};
-    global[key].skip = global[key].only = () => {};
+  const cp = require("child_process");
+  const cmd = findMochaCommand();
+  const proc = cp.exec(`${ cmd } ${ process.argv[1] }`, {
+    env: Object.assign({}, process.env, { NODE_ENV: "development" }, ENV)
   });
 
-  process.nextTick(() => {
-    const cp = require("child_process");
-    const cmd = findMocha();
-    const proc = cp.exec(`${ cmd } ${ process.argv[1] }`, {
-      env: Object.assign({}, process.env, { NODE_ENV: "development" }, ENV)
-    });
-
-    proc.stdout.pipe(process.stdout);
-    proc.stderr.pipe(process.stderr);
-    proc.on("exit", (code) => {
-      process.exit(code);
-    });
+  proc.stdout.pipe(process.stdout);
+  proc.stderr.pipe(process.stderr);
+  proc.on("exit", (code) => {
+    process.exit(code);
   });
 }
-
-runWithMocha();
 
 module.exports = (env) => {
   Object.assign(ENV, env);
 };
+
+if (!isMocha()) {
+  setupMochaAPI();
+  process.nextTick(runWithMocha);
+}
